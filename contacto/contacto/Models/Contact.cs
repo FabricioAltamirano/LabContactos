aa6f78672c653cd66d9f34c7cde2dcae175bc2f1
faltamirano98@hotmail.com <faltamirano98@hotmail.com>
2019-08-23 20:36:08 -0400

    con modelo

---
+++ contacto/contacto/Models/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace contacto.Models
+{
+    public enum TypeContact
+    {
+        telephone,
+        email,
+        facebook,
+    }
+    public class Contact
+    {
+        [Key]
+        public int  ContactID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public TypeContact Type { get; set; }
+        [Required]
+        [Display(Description ="Contact")]
+        public string ContactValue { get; set; }
+    }
+}
